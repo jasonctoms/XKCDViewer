ecc2c2d7a532683c1867e354f7afa795ffee1923
Jason Toms <jason.toms@nrk.no>
2020-11-14 15:10:49 +0100

    Add go to specific comic functionality

--- app/src/main/java/com/jorbital/xkcdcviewer/ui/browse/BrowseFragment.kt
+++ app/src/main/java/com/jorbital/xkcdcviewer/ui/browse/BrowseFragment.kt
@@ -2,11 +2,15 @@ package com.jorbital.xkcdcviewer.ui.browse
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
 import com.jorbital.service.data.ComicDto
 import com.jorbital.xkcdcviewer.R
 import com.jorbital.xkcdcviewer.databinding.BrowseFragmentBinding
+import com.jorbital.xkcdcviewer.extensions.afterTextChanged
 import com.jorbital.xkcdcviewer.extensions.observe
 import com.jorbital.xkcdcviewer.extensions.viewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -30,11 +34,43 @@ class BrowseFragment : Fragment(R.layout.browse_fragment) {
         viewModel.getLatestComic()
         binding.nextButton.setOnClickListener { viewModel.getNextComic() }
         binding.previousButton.setOnClickListener { viewModel.getPreviousComic() }
+        binding.goToButton.setOnClickListener { openGoToDialog() }
+    }
+
+    private fun openGoToDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(getString(R.string.go_to_specific_comic))
+        val dialogLayout = layoutInflater.inflate(R.layout.go_to_comic_dialog, null)
+        val inputLayout = dialogLayout.findViewById<TextInputLayout>(R.id.text_field)
+        inputLayout.editText?.afterTextChanged {
+            if (isValidComicNumber(it) || it.isBlank()) {
+                inputLayout.error = null
+            } else {
+                inputLayout.error = getString(R.string.enter_a_valid_number)
+            }
+        }
+        builder.setView(dialogLayout)
+        builder.setPositiveButton("OK") { _, _ ->
+            val text = inputLayout.editText?.text
+            if (inputLayout.error == null && !text.isNullOrBlank()) {
+                viewModel.getSpecificComic(text.toString().toInt())
+            } else if (!text.isNullOrBlank()) {
+                Toast.makeText(context, getString(R.string.enter_a_valid_number), Toast.LENGTH_SHORT).show()
+            }
+        }
+        builder.show()
+    }
+
+    private fun isValidComicNumber(number: String): Boolean {
+        number.toIntOrNull()?.let {
+            if (it > 0 && it <= viewModel.latestComicNumber) return true
+        }
+        return false
     }
 
     private fun populateComicView(comic: ComicDto) {
         binding.nextButton.isEnabled = viewModel.currentComicNumber < viewModel.latestComicNumber
-        binding.previousButton.isEnabled = viewModel.currentComicNumber >= 1
+        binding.previousButton.isEnabled = viewModel.currentComicNumber > 1
 
         binding.comicView.populateView(comic)
     }
